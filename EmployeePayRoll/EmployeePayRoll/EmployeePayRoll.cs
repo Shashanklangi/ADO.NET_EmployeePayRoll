e422e4b68cc081953ca692b7fdc272d549a4752c
Shashank <langishashank@gmail.com>
2022-06-18 10:40:06 +0530

    [Shashank]: Eshtablishing and Closing the Connection.

---
+++ EmployeePayRoll/EmployeePayRoll/EmployeePayRoll.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayRoll
+{
+    public class EmployeePayRoll
+    {
+        /// <summary>
+        /// UC1:Check connection is established or not
+        /// </summary>
+        static string connectionstring = @"Data source=(localdb)\MSSQLLocalDB;Initial Catalog=EmployeeServices;Integrated Security=SSPI";
+        static SqlConnection connection = new SqlConnection(connectionstring);
+
+        public void EstablishConnection()
+        {
+            if (connection != null && connection.State.Equals(ConnectionState.Closed))
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch (Exception)
+                {
+                    throw new EmployeeException(EmployeeException.ExceptionType.CONNECTION_FAILED, "Connection Not Found");
+                }
+            }
+        }
+        public void CloseConnection()
+        {
+            if (connection != null && connection.State.Equals(ConnectionState.Open))
+            {
+                try
+                {
+                    connection.Close();
+                }
+                catch (Exception)
+                {
+                    throw new EmployeeException(EmployeeException.ExceptionType.CONNECTION_FAILED, "Connection Not Found");
+                }
+            }
+        }
+    }
+}
---
+++ EmployeePayRoll/EmployeePayRoll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayRoll
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Pay Roll Service");
+        }
+    }
+}
